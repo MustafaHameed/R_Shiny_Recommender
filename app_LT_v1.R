ac01f133be0fc423ed14dcf3279a1316e3e7bd26
James Topor <jim.topor@gmail.com>
2017-07-13 10:54:25 -0400

    Updated with data loading + genre list + artist list code

--- app_LT_v1.R
+++ app_LT_v1.R
@@ -8,15 +8,64 @@ lfm_art <- read_delim("https://raw.githubusercontent.com/jtopor/CUNY-MSDA-643/ma
 # cleanup foreign characters in artist names: most will be converted to '?'
 lfm_art$name <- iconv(lfm_art$name, from = "UTF-8", to = "ASCII//TRANSLIT")
 
+# load last.fm tags.dat file to get access to genre names
+lfm_tags <- read_delim("https://raw.githubusercontent.com/jtopor/CUNY-MSDA-643/master/FP/tags.dat", delim = "\t")
+
+
 # load required matrices  
 
-ag_mat <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/ag_mat.csv")  
+# ------------------------
+# load artist-genre matrix
+ag_mat <- as.matrix(read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/ag_mat.csv", check.names = FALSE,
+                             header=TRUE, sep = ",", stringsAsFactors = FALSE) )
 
-art_sim <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/art_sim.csv")  
+# set rownames to values in V1
+row.names(ag_mat) <- as.numeric(ag_mat[,1])
 
-last_sm <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/last_sm.csv")  
+# now truncate matrix to eliminate col 1
+ag_mat <- ag_mat[,2:ncol(ag_mat)]
+# --------------------------
 
-tenrecs <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/user_tenrecs.csv")  
+
+# ----------------------------
+# load artist similarity matrix
+art_sim <- as.matrix(read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/art_sim.csv", check.names = FALSE,
+                              header=TRUE, sep = ",", stringsAsFactors = FALSE) )
+
+# set rownames to values in V1
+row.names(art_sim) <- as.numeric(art_sim[,1])
+
+# now truncate matrix to eliminate col 1
+art_sim <- art_sim[,2:ncol(art_sim)]
+# ----------------------------
+
+last_sm <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/last_sm.csv", 
+                    header=TRUE, sep = ",", stringsAsFactors = FALSE)
+
+tenrecs <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/user_tenrecs.csv", 
+                    header=TRUE, sep = ",", stringsAsFactors = FALSE)
+
+
+###### Get the top 815 Artists IDS ---------------
+
+# extract the 815 top artist IDs from art_sim matrix and convert to numeric
+artistIDs <- as.numeric(rownames(art_sim) )
+
+# then get associated genre names from lfm_art data frame
+a_names <- lfm_art[lfm_art$id %in% artistIDs,]$name
+#------------------------------------------------
+
+
+###### Get the list of the top 200 genre names ----
+
+# extract the genre tagIDs from artist-genre matrix and convert to numeric
+tagIDs <- as.numeric(colnames(ag_mat))
+
+# then get associated genre names from lfm_tags data frame
+g_names <- lfm_tags[lfm_tags$tagID %in% tagIDs,]$tagValue
+# --------------------------------------------------
+
+
 
 # ____ui____
 
--- app_version_1.R
+++ app_version_1.R
@@ -5,9 +5,16 @@ library(shiny)
 library(dplyr)
 library(readr)
 
+# load artists.data file - has artists names in it
 lfm_art <- read_delim("https://raw.githubusercontent.com/jtopor/CUNY-MSDA-643/master/FP/artists.dat", delim = "\t") %>% select(id, name)
 lfm_art$name <- iconv(lfm_art$name, from = "UTF-8", to = "ASCII//TRANSLIT")
 
+# load last.fm tags.dat file - has list of genre names
+lfm_tags <- read_delim("https://raw.githubusercontent.com/jtopor/CUNY-MSDA-643/master/FP/tags.dat", delim = "\t")
+
+
+# ----------------------------
+# load artist similarity matrix
 art_sim <- as.matrix(read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/art_sim.csv", check.names = FALSE,
                               header=TRUE, sep = ",", stringsAsFactors = FALSE) )
 
@@ -16,17 +23,60 @@ row.names(art_sim) <- as.numeric(art_sim[,1])
 
 # now truncate matrix to eliminate col 1
 art_sim <- art_sim[,2:ncol(art_sim)]
+# -----------------------------
 
 
+# ------------------------
+# load artist-genre matrix
+ag_mat <- as.matrix(read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/ag_mat.csv", check.names = FALSE,
+                             header=TRUE, sep = ",", stringsAsFactors = FALSE) )
 
-autocomplete_list <- c(lfm_art$name)
+# set rownames to values in V1
+row.names(ag_mat) <- as.numeric(ag_mat[,1])
+
+# now truncate matrix to eliminate col 1
+ag_mat <- ag_mat[,2:ncol(ag_mat)]
+# --------------------------
+
+# load UBCF recs for each user (10 artists recommendations per user)
+user_tenrecs <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/user_tenrecs.csv", 
+                         header=TRUE, sep = ",", stringsAsFactors = FALSE)
+
+
+# load user-artist data frame: to be used for displaying list of artists
+# a user has listened to.
+last_sm <- read.csv("https://raw.githubusercontent.com/RobertSellers/R_Shiny_Recommender/master/R-Obj-CSVs/last_sm.csv", 
+                    header=TRUE, sep = ",", stringsAsFactors = FALSE)
+
+
+###### Get the top 815 Artists IDS ---------------
+
+# extract the 815 top artist IDs from art_sim matrix and convert to numeric
+artistIDs <- as.numeric(rownames(art_sim) )
+
+# then get associated genre names from lfm_art data frame
+a_names <- lfm_art[lfm_art$id %in% artistIDs,]$name
+#------------------------------------------------
+
+
+###### Get the list of the top 200 genre names ----
+
+# extract the genre tagIDs from artist-genre matrix and convert to numeric
+tagIDs <- as.numeric(colnames(ag_mat))
+
+# then get associated genre names from lfm_tags data frame
+g_names <- lfm_tags[lfm_tags$tagID %in% tagIDs,]$tagValue
+# --------------------------------------------------
+
+# initialize autocomplete list for artist names
+autocomplete_list <- c(a_names)
 
 ui <- shinyUI(
   fluidPage(tags$style(type="text/css",".shiny-output-error { visibility: hidden; }",".shiny-output-error:before { visibility: hidden; }"),
             tags$style(type="text/css","#search { top: 50% !important;left: 50% !important;margin-top: -100px !important;margin-left: -250px 
                        !important; color: blue;font-size: 20px;font-style: italic;}"),         
             
-            titlePanel("Movie Recommendation Engine"),
+            titlePanel("Musical Artist Recommender"),
             fluidRow(
               column(5,
               # one way of doing it
